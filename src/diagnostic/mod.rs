4a6d8814df6c39c0c2f402f9e9805e43a254ecac
newcomb-luke <newcomb.luke@gmail.com>
2022-01-28 09:20:43 -0500

    Added preliminary lexer architecture

---
+++ src/diagnostic/mod.rs
@@ -0,0 +1,4 @@
+/// Represents the source of a Token, which could be source code itself, or something like a macro
+/// expansion in order to facilitate good diagnostics
+#[derive(Debug)]
+pub struct Source;
---
+++ src/lexer/token.rs
@@ -0,0 +1,33 @@
+use crate::diagnostic::Source;
+use logos::Logos;
+
+/// An Enum that represents a token as provided by Logos, which will later be converted into the
+/// regular Token, but still stores which kind of token it is, hence the name
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Logos)]
+pub enum TokenKind {
+    #[regex("[0-9]+")]
+    LiteralInteger,
+
+    #[regex("[ \t]+")]
+    Whitespace,
+
+    #[token("\n")]
+    Newline,
+
+    #[error]
+    ErrorGeneric,
+}
+
+/// A token that has been produced by the lexer from C source code
+#[derive(Debug, Clone, Copy)]
+pub struct Token<'a, 'b> {
+    // The kind of token
+    pub kind: TokenKind,
+
+    // A reference to the source that this token belongs to, this could be a file or something like
+    // a macro expansion
+    pub source: &'a Source,
+
+    // The text that this token represents, a section of a source file
+    pub text: &'b str,
+}
