8941148ca39a001081911470d25e90fa81b53210
Troy Neubauer <troyneubauer@gmail.com>
2022-02-25 09:42:40 -0500

    Python CI #4

--- .github/scripts/compile_python.sh
+++ .github/scripts/compile_python.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-pwd
-ls
 rustup update stable && rustup default stable
 cargo build --release
 git clone https://github.com/python/cpython.git --depth 1
-export "CC=$PWDtarget/debug/sacc"
+export CC=$(pwd)/target/debug/sacc
 echo $CC
 cd cpython
-ls
+./configure && make -j8
